176ff46f6ec78b9e3850b47c907cfb58a47024a2
saimomCoreDevs <saimom_islam@coredevs.ltd>
2021-09-23 17:32:07 +0600

    Employee done

--- server/Router/parcelRouter.js
+++ server/Router/parcelRouter.js
@@ -10,10 +10,17 @@ const toId = mongoose.Types.ObjectId
 
 const newParcel = async (req,res)=>{
     try{
+        // const {ProductType,SenderName,SenderNumber,SenderEmail,RecieverName,RecieverNumber,RecieverEmail,Totalcost,PaidAmount,PayableAmount,status,SearchId} = req.body
         req.params.send = toId(req.params.send)
-        if(!req.params.send) returnres.status(400).json({message:"Branch Does Not Exist"})
+        if(!req.params.send) return res.status(400).json({message:"Branch Does Not Exist"})
         req.params.to = toId(req.params.to)
         if(!req.params.send) return res.status(400).json({message:"Branch Does Not Exist"})
+//         var dateobj = 
+//    new Date('October 15, 1996 05:35:32');
+        // if(!ProductType|| !SenderName||!SenderNumber||!SenderEmail||!RecieverName||!RecieverNumber||!RecieverEmail||!Totalcost||!SearchId||!PaidAmount||!PayableAmount||!status) return res.status(400).send({
+        //     check:SenderName
+        // })
+        // .json({message:"All Field Must Be Filled Up"})
         parcel = new Parcel(req.body);
         parcel.BookedFrom =req.params.send
         parcel.SendTo =req.params.to
@@ -108,6 +115,14 @@ console.log(obj)
           });
       }
   });
+}
+
+const comingProduct = async(req,res)=>{
+    const id = req.params.id
+    const result = await Parcel.find({SendTo:id}) 
+    if(!result) return res.status(400).json({message:"No Parcel Available"})
+    res.status(200).send(result)
+
 }
 
 //GET 'http://localhost:4000/parcelApi/deleteParcel/getData'
@@ -273,10 +288,432 @@ const getData = async(req,res)=>{
   })
    
 }
+
+const BookedBranchData = async(req,res)=>{
+    const status = req.params.status
+    const branchId = req.params.branchId
+    console.log(branchId)
+    const testing = toId(branchId)
+    console.log(testing)
+    const d = new Date().toISOString()
+        console.log(d)
+        var test = d.split('T')
+        console.log(test)
+        if(status==="Booked"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        BookedFrom: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Booked:{$sum:1},
+                        data: { $push: '$$ROOT' }
+                        // detail: { $first: '$$ROOT' }
+                    }
+                   
+                },
+                // {
+                //     $replaceRoot: {
+                //       newRoot: { $mergeObjects: [{ Booked: '$Booked' }, '$detail'] },
+                //     }
+                // }
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Booked :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==="Recieved"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        BookedFrom: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Recieved:{$sum:1},
+                        data: { $push: '$$ROOT' }
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Recieved :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==="Sent"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        BookedFrom: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Sent:{$sum:1},
+                        data:{$push:"$$ROOT"}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Sent :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==='Delivered'){
+            
+                result = await Parcel.aggregate([
+                    {
+                        $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                    },
+                    {
+                        $match:{
+                            status:status,
+                            creationDate:test[0],
+                            BookedFrom: testing
+                            }
+                    }, 
+              
+                    {
+                        $group:{
+                            _id:null,     
+                            Delivered:{$sum:1},
+                            data:{$push:'$$ROOT'}
+                        }
+                       
+                    },
+                    {
+                        $project: {
+                          _id:0,
+                          data:1,
+                          Booked :1
+                        }
+                      }
+             
+                ]
+                )
+                res.status(200).send(result)
+            }
+        else{
+            
+                result = await Parcel.aggregate([
+                    {
+                        $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$expectedDate"}}}
+                    },
+                    {
+                        $match:{
+                            creationDate:test[0],
+                            BookedFrom: testing
+                            }
+                    }, 
+              
+                    {
+                        $group:{
+                            _id:null,     
+                            Upcoming:{$sum:1},
+                            data:{$push:'$$ROOT'}
+                        }
+                       
+                    },
+                    {
+                        $project: {
+                          _id:0,
+                          data:1,
+                          Upcoming :1
+                        }
+                      }
+             
+                ]
+                )
+                res.status(200).send(result)
+            }
+}
+
+
+const SentBranchData = async(req,res)=>{
+    const status = req.params.status
+    const branchId = req.params.branchId
+    console.log(branchId)
+    const testing = toId(branchId)
+    console.log(testing)
+    const d = new Date().toISOString()
+        console.log(d)
+        var test = d.split('T')
+        console.log(test)
+        if(status==="Booked"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        SendTo: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Booked:{$sum:1},
+                        data:{$push:'$$ROOT'}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Booked :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==="Recieved"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        SendTo: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Recieved:{$sum:1},
+                        data:{$push:'$$ROOT'}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Recieved :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==="Sent"){
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        status:status,
+                        creationDate:test[0],
+                        SendTo: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Sent:{$sum:1},
+                        data:{$push:'$$ROOT'}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Booked :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        }
+        else if(status==="Delivered"){
+            
+                result = await Parcel.aggregate([
+                    {
+                        $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                    },
+                    {
+                        $match:{
+                            status:status,
+                            creationDate:test[0],
+                            SendTo: testing
+                            }
+                    }, 
+              
+                    {
+                        $group:{
+                            _id:null,     
+                            Delivered:{$sum:1},
+                            data:{$push:'$$ROOT'}
+                        }
+                       
+                    },
+                    {
+                        $project: {
+                          _id:0,
+                          data:1,
+                          Delivered :1
+                        }
+                      }
+             
+                ]
+                )
+                res.status(200).send(result)
+            }
+        else{
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$expectedDate"}}}
+                },
+                {
+                    $match:{
+                        creationDate:test[0],
+                        BookedFrom: testing
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Upcoming:{$sum:1},
+                        data:{$push:'$$ROOT'}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Upcoming :1
+                    }
+                  }
+         
+            ]
+            )
+            res.status(200).send(result)
+        
+        }
+   
+}
+
+const SelfData = async(req,res)=>{
+    
+    var userId = req.params.userId
+    userId = toId(userId)
+    const time = req.params.date
+    var d = new Date(time)
+    d= d.getTime()
+     d = new Date(d).toJSON();
+        console.log(d)
+        var test = d.split('T')
+        console.log(test)
+            result = await Parcel.aggregate([
+                {
+                    $addFields: { creationDate:  {$dateToString:{format: "%Y-%m-%d", date: "$createdAt"}}}
+                },
+                {
+                    $match:{
+                        creationDate:test[0],
+                        BookedBy:userId
+                        }
+                }, 
+          
+                {
+                    $group:{
+                        _id:null,     
+                        Count:{$sum:1},
+                        data:{$push:'$$ROOT'}
+                    }
+                   
+                },
+                {
+                    $project: {
+                      _id:0,
+                      data:1,
+                      Count:1
+                    }
+                  }
+                
+         
+            ]
+            )
+            res.status(200).send(result)      
+        
+}
+
+
+const generateProductId =async (req,res)=>{
+    let r = (Math.random() + 1).toString(36).substring(7);
+    console.log(typeof(r));
+    const parcel = await Parcel.findOne({ProductType:r})
+    if(parcel) return res.status(400).json({mesage:"This Id Is already Exists.Click Again to get new Id"})
+    return res.status(200).send({
+        searchId:r
+    })
+}
 router.route('/parcelApi/parcel/:send/:to/:employee')
     .post(newParcel)
 router.route('/parcelApi/see/:parcelid/:employeeid')
     .get(getParcel)
+router.route('/parcelApi/seeComingProduct/:id')
+    .get(comingProduct)
 router.route('/parcelApi/see/allParcel')
     .get(allParcel)
 router.route('/parcelApi/updateParcel/:id')
@@ -287,24 +724,15 @@ router.route('/parcelApi/deleteParcel/sorted')
     .get(sortedData)
 router.route('/parcelApi/deleteParcel/getData/:time')
     .get(getData)
+router.route('/parcelApi/branchUser/bookedFrom/:branchId/:status')
+    .get(BookedBranchData)
+router.route('/parcelApi/branchUser/sendTo/:branchId/:status')
+    .get(SentBranchData)
+router.route('/parcelApi/branchUser/:userId/:date')
+    .get(SelfData)
+router.route('/parcelApi/generateId')
+    .get(generateProductId)
+
 
 module.exports = router;
 
-
-// router.route('/parcelApi/:parcel/:send/:to')
-//     .get(getParcel)
-
-// {
-// 	"products":[{"ProductType":"Electrtic"},{"ProductType":"Books"}],
-// 	"SenderName":"xyz",
-// 	"SenderNumber":"5656",
-// 	"RecieverName":"oml",
-// 	"RecieverNumber":"025",
-// 	"TotalCost":"65656",
-// 	"PaidAmount":"554",
-// 	"PayableAmount":"55",
-// 	"status":"Booked"
-// }
-
-// see parcel
-// http://localhost:4000/see/61417fcd07a88871a93540ef/61417a45144ae021eedd1b4c
